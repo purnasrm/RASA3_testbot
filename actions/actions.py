78e4b0b9f121a4565294b5e5b9892bd99385c45f
Purna Sampath Rathnayake <rmpsr91@gmail.com>
2023-01-04 21:50:10 +0530

    list in response tested

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,37 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionShowList(Action):
+
+    def name(self) -> Text:
+        return "action_show_list"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        list_uppercase = ['A', 'B', 'C']
+        list_lowercase = ['a', 'b', 'c']
+
+        upper_case_string = ""
+        lower_case_string = ""
+
+        for letter in list_uppercase:
+            upper_case_string += letter + "\n"
+
+        for letter in list_lowercase:
+            lower_case_string += letter + "\n"
+
+        dispatcher.utter_message(
+            response="utter_list",
+            upper_case_list=upper_case_string,
+            lower_case_list=lower_case_string
+        )
+
+        return []
