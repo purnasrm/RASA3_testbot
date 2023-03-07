bfd5ae506d988c9083f3563ec47f12983c21f3b6
Purna Sampath Rathnayake <rmpsr91@gmail.com>
2023-03-07 17:08:55 +0100

    tested

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import ActionExecuted, UserUttered
 
 
 class ActionHelloWorld(Action):
@@ -16,5 +17,15 @@ class ActionHelloWorld(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(text="Hello World!")
+        data = {
+            "intent": {
+                "name": "bot_challenge",
+                "confidence": 1.0,
+            }
+        }
+        
+        return [
+            ActionExecuted("action_listen"),
+            UserUttered(text="/bot_challenge", parse_data=data)
+        ]
 
-        return []
